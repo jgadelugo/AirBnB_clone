ac398b3f84c6c2e144d18adf393fded082f78fcd
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-11-09 20:57:08 +0000

    [FEATURE] Base for test base

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Tests for class base_model """
+import unittest
+
+from models.base_model import BaseModel as Base
+
+
+class TestBaseModel(unittest.TestCase):
+    """ tests for class base """
+
+        def test_id(self):
+            """ test id """
