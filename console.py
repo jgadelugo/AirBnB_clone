aac85357b656f6dc617716c06369232ea3e46d2b
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-11-11 20:36:21 +0000

    [FEATURE] Add command interpreter using cmd module

--- console.py
+++ console.py
@@ -4,18 +4,23 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ """
+    """ entry point of the command interpreter """
 
+    prompt = "(hbnb) "
 
-    prompt = "(hbnb)"
-    
     def do_quit(self, args):
         """ Quits the program """
         return True
 
+    def emptyline(self):
+        pass
+
     def do_EOF(self, args):
         """ Reads EOF and exits """
         return True
+
+    def help_quit(self):
+        print("Quit command to exit the program\n")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
