e70f913ae9b9e59ef70799bec500950f3462b39f
Mitchell <mmoscovics@yahoo.com>
2019-11-13 19:27:33 +0000

    [Create] base structure for file storage testing

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Tests for class FileStorage """
+
+import unittest
+from models.engine.file_storage import FileStorage
+import json
+
+
+class TestFileStorage(unittest.TestCase):
+    """ Tests for class FileStorage """
+    pass
